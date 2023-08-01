4ddfc78e8bd4fb05caea99d86bdf8acf5695c4e9
SATYASARAN CHANGDAR <39429398+satyasaran@users.noreply.github.com>
2023-08-01 16:57:04 +0200

    Create GradCampy.py

---
+++ GradCampy.py
@@ -0,0 +1,143 @@
+import argparse
+import os
+import cv2
+import numpy as np
+import torch
+from torchvision import models
+from pytorch_grad_cam import (
+    GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus,
+    AblationCAM, XGradCAM, EigenCAM, EigenGradCAM,
+    LayerCAM, FullGrad, GradCAMElementWise
+)
+from pytorch_grad_cam import GuidedBackpropReLUModel
+from pytorch_grad_cam.utils.image import (
+    show_cam_on_image, deprocess_image, preprocess_image
+)
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+
+# Define the custom model class
+class CustomModel(torch.nn.Module):
+    def __init__(self):
+        super(CustomModel, self).__init__()
+        self.model = models.resnet50(pretrained=False)
+        num_features = self.model.fc.in_features
+        self.model.fc = torch.nn.Linear(num_features, 2)  # 2 classes
+        pretrained_path = '/content/drive/MyDrive/AI_health/data_health/2class_resnet50_30epochs.pth'
+        self.model.load_state_dict(torch.load(pretrained_path))
+        self.model.eval()
+
+    def forward(self, x):
+        return self.model(x)
+
+# Parse command-line arguments
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-cuda', action='store_true', default=False,
+                        help='Use NVIDIA GPU acceleration')
+    parser.add_argument(
+        '--image-path',
+        type=str,
+        default='./examples/both.png',
+        help='Input image path')
+    parser.add_argument('--aug-smooth', action='store_true',
+                        help='Apply test time augmentation to smooth the CAM')
+    parser.add_argument(
+        '--eigen-smooth',
+        action='store_true',
+        help='Reduce noise by taking the first principle component'
+        'of cam_weights*activations')
+    parser.add_argument('--method', type=str, default='gradcam',
+                        choices=[
+                            'gradcam', 'hirescam', 'gradcam++',
+                            'scorecam', 'xgradcam', 'ablationcam',
+                            'eigencam', 'eigengradcam', 'layercam',
+                            'fullgrad', 'gradcamelementwise'
+                        ],
+                        help='CAM method')
+    parser.add_argument('--output-dir', type=str, default='output',
+                        help='Output directory to save the images')
+    args = parser.parse_args()
+    args.use_cuda = args.use_cuda and torch.cuda.is_available()
+    if args.use_cuda:
+        print('Using GPU for acceleration')
+    else:
+        print('Using CPU for computation')
+    return args
+
+if __name__ == '__main__':
+    # Get command-line arguments
+    args = get_args()
+
+    # Create a dictionary to map method names to corresponding CAM algorithms
+    methods = {
+        "gradcam": GradCAM,
+        "hirescam": HiResCAM,
+        "scorecam": ScoreCAM,
+        "gradcam++": GradCAMPlusPlus,
+        "ablationcam": AblationCAM,
+        "xgradcam": XGradCAM,
+        "eigencam": EigenCAM,
+        "eigengradcam": EigenGradCAM,
+        "layercam": LayerCAM,
+        "fullgrad": FullGrad,
+        "gradcamelementwise": GradCAMElementWise
+    }
+
+    # Instantiate the custom model
+    model = CustomModel()
+
+    # Define the target layer for CAM visualization
+    target_layers = [model.model.layer4]
+
+    # Load and preprocess the input image
+    rgb_img = cv2.imread(args.image_path, 1)[:, :, ::-1]
+    rgb_img = np.float32(rgb_img) / 255
+    input_tensor = preprocess_image(rgb_img,
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+
+    # Set the target to None for classification models
+    targets = None
+
+    # Select the CAM algorithm based on the specified method
+    cam_algorithm = methods[args.method]
+    with cam_algorithm(model=model,
+                       target_layers=target_layers,
+                       use_cuda=args.use_cuda) as cam:
+
+        # Set batch_size for efficiency
+        cam.batch_size = 32
+
+        # Generate the grayscale CAM
+        grayscale_cam = cam(input_tensor=input_tensor,
+                            targets=targets,
+                            aug_smooth=args.aug_smooth,
+                            eigen_smooth=args.eigen_smooth)
+
+        # Take the first channel of the grayscale CAM (single-channel representation)
+        grayscale_cam = grayscale_cam[0, :]
+
+        # Overlay the CAM on the original image to get a heatmap
+        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+
+    # Generate the guided backpropagation map
+    gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
+    gb = gb_model(input_tensor, target_category=None)
+
+    # Prepare the CAM mask for guided backpropagation visualization
+    cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+    cam_gb = deprocess_image(cam_mask * gb)
+    gb = deprocess_image(gb)
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    # Save the CAM, guided backpropagation, and combined visualization images
+    cam_output_path = os.path.join(args.output_dir, f'{args.method}_cam.jpg')
+    gb_output_path = os.path.join(args.output_dir, f'{args.method}_gb.jpg')
+    cam_gb_output_path = os.path.join(args.output_dir, f'{args.method}_cam_gb.jpg')
+
+    cv2.imwrite(cam_output_path, cam_image)
+    cv2.imwrite(gb_output_path, gb)
+    cv2.imwrite(cam_gb_output_path, cam_gb)
